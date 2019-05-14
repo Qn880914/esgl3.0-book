b0fb012ee13b3868b244769c8951bf35a571b19e
lyy19910615 <986837786@qq.com>
2019-05-14 13:41:58 +0800

    3dTexture

---
+++ Scripts/Chapter 1 Introduction to OpenGL ES 3.0/New Feature/Texturing/3DTexture/Texture3DPreview.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texture3DPreview : MonoBehaviour
+{
+    [SerializeField] private Renderer m_Renderer;
+
+    [SerializeField] private Texture3D m_Texture3DPreview;
+
+    [SerializeField] private int m_SampleQuality;
+
+    [SerializeField] private float m_Density;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+        propertyBlock.SetInt("_SamplingQuality", m_SampleQuality);
+        propertyBlock.SetTexture("_MainTex", m_Texture3DPreview);
+        propertyBlock.SetFloat("_Density", m_Density);
+        m_Renderer.SetPropertyBlock(propertyBlock);
+
+    }
+}
